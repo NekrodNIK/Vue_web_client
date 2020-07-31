0f2f533b9acf5099eee9c82bfeaa44bb9a04fbd0
NekrodNIK <NP17210@yandex.ru>
2020-07-31 17:10:20 +0600

    1

---
+++ dist/precache-manifest.6ce8d4768d0578df8999a33ad23f6cfe.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "31687113eeba867578b7",
+    "url": "/css/app.8caa578c.css"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "e765ba1990c8ad65fdcfd5386e9eca6a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "31687113eeba867578b7",
+    "url": "/js/app.0752e8e1.js"
+  },
+  {
+    "revision": "9e83726d6b1a868d032f",
+    "url": "/js/chunk-vendors.bdc9172a.js"
+  },
+  {
+    "revision": "09ed883459d9db256ea3f79d3d469924",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
